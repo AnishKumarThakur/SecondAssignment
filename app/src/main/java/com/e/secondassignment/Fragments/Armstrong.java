c7ac47de863ec4f5f53c3f6ace4884418e7203db
AnishKumarThakur <anishthakur5577@gmail.com>
2019-12-10 17:04:48 +0545

    Armstrong Calculation

--- app/src/main/java/com/e/secondassignment/Fragments/Armstrong.java
+++ app/src/main/java/com/e/secondassignment/Fragments/Armstrong.java
@@ -8,13 +8,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.e.secondassignment.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class Armstrong extends Fragment {
+public class Armstrong extends Fragment implements View.OnClickListener {
+    private EditText etArmstrong;
+    private Button btnArmstrong;
 
 
     public Armstrong() {
@@ -26,7 +31,30 @@ public class Armstrong extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_armstrong, container, false);
+        View view = inflater.inflate(R.layout.fragment_armstrong, container, false);
+        etArmstrong = view.findViewById(R.id.etArmstrong);
+        btnArmstrong = view.findViewById(R.id.btnArmstrong);
+        btnArmstrong.setOnClickListener(this);
+        return view;
     }
 
+    @Override
+    public void onClick(View v) {
+        int c = 0, a, temp;
+        int n = Integer.parseInt(etArmstrong.getText().toString());
+        temp = n;
+
+        while (n > 0) {
+            a = n % 10;
+            n = n / 10;
+            c = c + (a * a * a);
+        }
+        if (temp == c) {
+            Toast.makeText(getActivity(), temp + " is a Armstrong number.", Toast.LENGTH_SHORT).show();
+
+        } else {
+            Toast.makeText(getActivity(), temp + " is a not Armstrong number.", Toast.LENGTH_SHORT).show();
+
+        }
+    }
 }
